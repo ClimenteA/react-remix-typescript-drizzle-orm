11f309aa6a98273108ed9509c4fa05a3ba9ba98f
ClimenteA <climente.alin@gmail.com>
2024-04-22 13:34:08 +0300

    feat: service details reponsive

--- app/routes/softgata/components/ServiceDetails.tsx
+++ app/routes/softgata/components/ServiceDetails.tsx
@@ -2,25 +2,25 @@
 
 export default function ServiceDetails() {
     return (
-        <div className="t-bg-gray-50 t-mt-20 p-6 t-bg-[url('/softgata/future-robot.jpg')] t-bg-contain t-bg-right">
+        <div className="t-bg-gray-50 t-mt-20 md:t-p-6 t-p-4 t-bg-[url('/softgata/future-robot.jpg')] t-bg-contain t-bg-right">
 
 
-            <div className="t-flex t-flex-col t-gap-24 my-6 t-min-w-[600px] container">
+            <div className="t-flex t-flex-col t-gap-24 my-6 container">
 
                 <div className="t-bg-gray-950 t-rounded-md p-6 t-max-w-[800px]">
-                    <h2 className="title is-3 t-text-white">
+                    <h2 className="title is-size-3-desktop is-size-4-tablet is-size-4-mobile t-text-white">
                         <i className="mr-4 bi bi-arrow-right"></i>
                         Business Automation
                     </h2>
-                    <p className="t-text-white t-text-lg">
+                    <p className="t-text-white lg:t-text-lg t-text-md">
                         Any repeatable, “robotic” and long term business process slows down your operations and your revenue.
                         No matter the domain of your business if it involves doing work on a computer
                         some of the tasks can be automated.
                     </p>
-                    <p className="t-text-white t-text-lg mt-6">
+                    <p className="t-text-white lg:t-text-lg t-text-md lg:t-mt-6 t-mt-4">
                         Here are 5 questions to ask about your business processes:
                     </p>
-                    <ul className="t-text-white t-text-lg mt-2">
+                    <ul className="t-text-white lg:t-text-lg t-text-md mt-2">
                         <li>- Does the process "feel" robotic?</li>
                         <li>- Are we moving and transforming data from one tool to another?</li>
                         <li>- Do we do most of our work in Microsoft Office (ex: excel, outlook), SAP, and/or other internal applications?</li>
@@ -29,20 +29,21 @@ export default function ServiceDetails() {
                     </ul>
                 </div>
 
+
                 <div className="t-bg-gray-950 t-rounded-md p-6 t-max-w-[800px]">
-                    <h2 className="title is-3 t-text-white">
+                    <h2 className="title is-size-3-desktop is-size-4-tablet is-size-4-mobile t-text-white">
                         <i className="mr-4 bi bi-arrow-right"></i>
                         SAAS Development
                     </h2>
-                    <p className="t-text-white t-text-lg">
+                    <p className="t-text-white lg:t-text-lg t-text-md">
                         We can execute custom web applications in the following technologies: Python with
                         Django, FastAPI, Flask, Typescript with React and Remix.run, MongoDb, PostgresDB,
                         Go with GoFiber, AI integrations, external API integrations - any CRUD data oriented web application.
                     </p>
-                    <p className="t-text-white t-text-lg mt-6">
+                    <p className="t-text-white lg:t-text-lg t-text-md lg:t-mt-6 t-mt-4">
                         Here are some principles that guide us:
                     </p>
-                    <ul className="t-text-white t-text-lg mt-2">
+                    <ul className="t-text-white lg:t-text-lg t-text-md mt-2">
                         <li>- Simple is better than complex;</li>
                         <li>- Done is better than perfect;</li>
                         <li>- Fast iterations;</li>
@@ -52,19 +53,19 @@ export default function ServiceDetails() {
                 </div>
 
                 <div className="t-bg-gray-950 t-rounded-md p-6 t-max-w-[800px]">
-                    <h2 className="title is-3 t-text-white">
+                    <h2 className="title is-size-3-desktop is-size-4-tablet is-size-4-mobile t-text-white">
                         <i className="mr-4 bi bi-arrow-right"></i>
                         App Integrations
                     </h2>
-                    <p className="t-text-white t-text-lg">
+                    <p className="t-text-white lg:t-text-lg t-text-md">
                         There is an tremendous amount of software already built.
                         Each one of them with its pros and cons. Before embarking in creating custom software solutions
                         is wise to investigate if existing solutions can be implemented in a business process. We can help you identify them.
                     </p>
-                    <p className="t-text-white t-text-lg mt-6">
+                    <p className="t-text-white lg:t-text-lg t-text-md lg:t-mt-6 t-mt-4">
                         Here is some free consulting:
                     </p>
-                    <ul className="t-text-white t-text-lg mt-2">
+                    <ul className="t-text-white lg:t-text-lg t-text-md mt-2">
                         <li>- Excel with VBA can do a lot - identify team members that can be proficient in excel;</li>
                         <li>- Excel is also pretty easy to break and hard to maintain - offer your team the posibility to automate their work.
                             Install on their working machines vscode + python with some popular libraries (offer bonuses on each automated task);
