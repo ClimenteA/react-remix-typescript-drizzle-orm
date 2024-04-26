62aefa4608a91bdf67b6fb9165782fa41afabf32
ClimenteA <climente.alin@gmail.com>
2024-04-26 13:00:34 +0300

    feat: reset form

--- app/routes/softgata._index/components/Contact.tsx
+++ app/routes/softgata._index/components/Contact.tsx
@@ -1,14 +1,21 @@
 import { Form } from "@remix-run/react"
+import { useEffect, useRef } from "react"
 
 export default function Contact({ messageSent = false }) {
+
+    let $form = useRef<HTMLFormElement>(null)
+
+    useEffect(() => {
+        if (messageSent) $form.current?.reset()
+    }, [messageSent])
 
     return (
         <div className="t-bg-white">
             <div className="container lg:t-py-48 t-py-32">
-                <Form method="post" className="box t-py-16 lg:t-px-12 t-px-6 t-bg-gray-50">
+                <Form ref={$form} method="post" className="box t-py-16 lg:t-px-12 t-px-6 t-bg-gray-50">
 
                     <h2 className="title is-size-2-desktop is-size-3-tablet is-size-4-mobile">
-                        {messageSent ? "Message received!" : "Let's get in touch!"}
+                        {messageSent ? "Message sent!" : "Let's get in touch!"}
                     </h2>
                     <p className="-t-mt-3">
                         {
@@ -17,28 +24,31 @@ export default function Contact({ messageSent = false }) {
                                 : "Provide and a short description of the issue you think we can be of help."
                         }</p>
 
+                    {
+                        messageSent ? null :
+                            <div>
+                                <div className="field t-mt-16 t-max-w-[370px]">
+                                    <label className="label">Your email</label>
+                                    <div className="control">
+                                        <input className="input is-dark is-medium" name="email" type="email" placeholder="youremail@company.com" />
+                                    </div>
+                                </div>
 
-                    <div className="field t-mt-16 t-max-w-[370px]">
-                        <label className="label">Your email</label>
-                        <div className="control">
-                            <input className="input is-dark is-medium" name="email" type="email" placeholder="youremail@company.com" />
-                        </div>
-                    </div>
+                                <div className="field">
+                                    <label className="label">Your message</label>
+                                    <div className="control">
+                                        <textarea className="textarea is-dark is-medium" name="message" placeholder="Write a short overview of the area you think we can help you."></textarea>
+                                    </div>
+                                </div>
 
-                    <div className="field">
-                        <label className="label">Your message</label>
-                        <div className="control">
-                            <textarea className="textarea is-dark is-medium" name="message" placeholder="Write a short overview of the area you think we can help you."></textarea>
-                        </div>
-                    </div>
-
-                    <div className="t-flex t-justify-end">
-                        <button type="submit" className="button t-bg-rose-600 t-text-white is-large mt-6">
-                            Send
-                            <i className="ml-2 bi bi-send"></i>
-                        </button>
-                    </div>
-
+                                <div className="t-flex t-justify-end">
+                                    <button type="submit" className="button t-bg-rose-600 t-text-white is-large mt-6">
+                                        Send
+                                        <i className="ml-2 bi bi-send"></i>
+                                    </button>
+                                </div>
+                            </div>
+                    }
                 </Form>
             </div>
         </div>
